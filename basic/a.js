25c7ad26a09f5b3fa402e78a8db200bad9f01c3d
ruanxinjun <83932134@qq.com>
2017-09-14 18:00:25 +0800

    12

---
+++ basic/a.js
@@ -0,0 +1,10 @@
+var md = {
+	add:function(a,b){
+		console.log(a+b);
+	},
+	mute:function(a,b){
+		console.log(a-b);
+	}
+};
+
+module.exports = md;
---
+++ basic/b.js
@@ -0,0 +1,4 @@
+var a = require('./a.js');
+
+a.add('a','b');
+a.mute(1,104);
